f0c6dec236eca583e865d9904e95dd261a2dc2ee
Nour Elabbasy <nour102600@gmail.com>
2021-03-25 20:54:44 -0400

    Parstagram Part 1

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "6RfWOQuq8Y9P84SqnulCw6scRq1OL3bQKe4c65BS"
+                    $0.clientKey = "lxYXnbwO0aWyk1y7Mie1q3KQ1nHDKfvfwVUM9y5J"
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
         return true
     }
 
